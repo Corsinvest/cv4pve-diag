406231c1fdcadd5382c48bd52c48397010c1bc2b
daniele.corsini@corsinvest.it <daniele.corsini@corsinvest.it>
2023-05-05 19:12:12 +0200

    Improve build

--- src/Corsinvest.ProxmoxVE.Diagnostic/build.ps1
+++ src/Corsinvest.ProxmoxVE.Diagnostic/build.ps1
@@ -1,5 +1,5 @@
-# SPDX-FileCopyrightText: Copyright Corsinvest Srl
 # SPDX-License-Identifier: GPL-3.0-only
+# SPDX-FileCopyrightText: 2019 Copyright Corsinvest Srl
 
 [System.Console]::Clear();
 
@@ -16,17 +16,22 @@ Write-Output "
  == Build System
  ========================================================="
 
-$pathNet = "Bin\Release\net6.0"
+$pathNet = "Bin\Release\net7.0"
 
 Remove-Item -Path ".\$pathNet"  -Recurse -Force
 
 $rids = @("linux-x64", "linux-arm", "linux-arm64", "osx-x64", "win-x86", "win-x64", "win-arm", "win-arm64")
 foreach ($rid in $rids) {
-    dotnet publish -r $rid -c Release /p:PublishSingleFile=true --self-contained /p:EnableCompressionInSingleFile=true
+    dotnet publish -r $rid -c Release /p:PublishSingleFile=true --self-contained #/p:EnableCompressionInSingleFile=true
     $path = "$pathNet\$rid\publish\"
 
-    $fileName = Get-ChildItem $path -Exclude *.pdb -name
+    $fileName = Get-ChildItem $path -Exclude *.pdb, *.xml -name
     $fileDest = "$pathNet\$fileName-$rid.zip"
     Remove-Item $fileDest -ErrorAction SilentlyContinue
     Compress-Archive $path\$fileName $fileDest
+
+    Remove-Item "$pathNet\$rid" -Recurse -Force
 }
+
+Remove-Item "bin\debug" -Recurse -Force
+Remove-Item "obj" -Recurse -Force
